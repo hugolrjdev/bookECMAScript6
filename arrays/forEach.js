0c58cbb092f20a1fb7ea38b4f7691539192bbfbb
hugolrjdev <fortalezaplanodesaude@gmail.com>
2021-04-06 22:35:38 -0300

    forEach for book ECMAscript 6

---
+++ arrays/forEach.js
@@ -0,0 +1,12 @@
+var nomes = ["Hugo", "Thuany", "Valentina", "Neto"]
+
+for (var i = 0; i < nomes.length; i++) {
+
+    console.log('for', nomes[i]);
+
+}
+
+nomes.forEach((name)=>{
+    console.log('forEach', name);
+});
+
